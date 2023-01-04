301900ff3dd4590a5969b98a43e5d70e44e585b0
bourg <dj.ka@outlook.de>
2023-01-04 19:59:54 +0100

    Added app.js

--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 
+const indexRouter = require('./routes/indexRouter');
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
+
+app.use('/', indexRouter);
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('layout', 'layouts/default.ejs');
 
-app.get('/', (req, res, next) => {
+app.get('/', (req ,res, next) => {
     res.render('index.ejs');
 })
 
